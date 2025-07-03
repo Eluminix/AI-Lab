01fe007249d50b22e07f73afa51603ce64d5904a
wielgola <39047784+wielgola@users.noreply.github.com>
2025-07-03 11:20:03 +0200

    Cleanup

--- AI_LAB_XLM_ROBERTA/ai_lab.py
+++
@@ -1,9 +0,0 @@
-from utils import extract_data_from_raw
-
-# Beispielpfade – anpassen an eure Ordnerstruktur
-data_link_file = "data/train_links.csv"
-raw_data_dir = "jsons"
-manual_file = "manual_crawl.json"  # kann leer sein
-output_csv = "data/train.csv"
-
-extract_data_from_raw(data_link_file, raw_data_dir, manual_file, output_csv)
